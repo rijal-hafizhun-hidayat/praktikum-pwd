4f72d1ec0a4a992ddbd0c9a3bfaad60f7945de0e
Rijal Hafizhun Hidayat <62733019+rijal-hafizhun-hidayat@users.noreply.github.com>
2021-11-23 17:17:21 +0700

    Add files via upload

---
+++ pertemuan_6/form_user.php
@@ -0,0 +1,12 @@
+<?php 
+    echo "<h2>tambah user</h2>
+    <form method = post action = input_user.php>
+    <table>
+    <tr><td>username</td><td> : <input name = 'id_user' type = 'text'></td></tr>
+    <tr><td>password</td><td> : <input name = 'password' type = 'password'></td></tr>
+    <tr><td>nama lengkap</td><td> : <input name = 'nama' type = 'text'></td></tr>
+    <tr><td>email</td><td> : <input name = 'email' type = 'text'></td></tr>
+    <tr><td colspan = 2><input type = 'submit' value = 'SIMPAN'></td></tr>
+    </table>
+    </form>";
+?>
